6a62698e204e036443e2f1a848b8bfbc73df434c
elly123 <xiefang111222@126.com>
2014-10-16 13:20:56 +0800

    add validate empty object for New Item

--- js/angularList.js
+++ js/angularList.js
@@ -1,21 +1,19 @@
-(function(){
-    var app = angular.module('angularList',[]);
-    app.controller('listController',function(){
-        this.items = listItems;
-        this.remove = function(item){
-            this.items.splice(this.items.indexOf(item),1);
+(function () {
+    var app = angular.module('angularList', []);
+    app.controller('listController', function () {
+        this.items = [];
+        this.remove = function (item) {
+            this.items.splice(this.items.indexOf(item), 1);
         };
 
     });
-    app.controller('addController',function(){
+    app.controller('addController', function () {
         this.item = {};
-        this.addItem = function(list) {
-            list.items.push(this.item);
-            this.item = {};
+        this.addItem = function (list) {
+            if (!jQuery.isEmptyObject(this.item)) {
+                list.items.push(this.item);
+                this.item = {};
+            }
         };
     });
-    var listItems = [
-        {
-            itemName: "itemOne"
-        }];
 })();
